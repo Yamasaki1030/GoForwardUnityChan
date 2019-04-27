b589664e21b339ab6edb89e5e2435b219a426280
Yamasaki1030 <49091177+Yamasaki1030@users.noreply.github.com>
2019-04-27 20:01:20 +0900

    GoForwardUnityChanプロジェクトのコミット
    Lesson7の課題提出

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour {
+
+    // テキスト
+    private GameObject gameOverText;
+    private GameObject runLengthText;
+    // 走行距離
+    private float len = 0;
+    // 速度
+    private float speed = 0.03f;
+    // ゲームオーバーの判定
+    private bool isGameOver = false;
+
+	// Use this for initialization
+	void Start () {
+        // シーンビューからオブジェクトを検索
+        this.gameOverText = GameObject.Find("GameOver");
+        this.runLengthText = GameObject.Find("RunLength");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.isGameOver == false)
+        {
+            // 走行距離の更新と表示
+            this.len += this.speed;
+            this.runLengthText.GetComponent<Text>().text = "Distance: " + len.ToString("F2") + "m";
+        }
+
+        // ゲームオーバーになった場合
+        if(this.isGameOver == true)
+        {
+            // クリックされたらシーンをロードする
+            if (Input.GetMouseButtonDown(0))
+            {
+                SceneManager.LoadScene("GameScene");
+            }
+        }
+	}
+
+    public void GameOver()
+    {
+        // ゲームオーバーを表示
+        this.gameOverText.GetComponent<Text>().text = "GameOver";
+        this.isGameOver = true;
+    }
+}
---
+++ Assets/Scripts/UnityChanController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanController : MonoBehaviour {
+
+    Animator animator;
+    Rigidbody2D rigid2D;
+
+    // 地面の位置
+    private float groundLevel = -3.0f;
+    // ジャンプ速度の減衰・ジャンプの速度
+    private float dump = 0.8f;
+    float jumpVelocity = 20;
+    // ゲームオーバーになる位置
+    private float deadLine = -9;
+
+	// Use this for initialization
+	void Start () {
+        // アニメータのコンポーネントを取得
+        this.animator = GetComponent<Animator>();
+
+        this.rigid2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // 走るアニメーションを再生
+        this.animator.SetFloat("Horizontal", 1);
+
+        // 着地しているかどうかを調べる
+        bool isGround = (transform.position.y > this.groundLevel) ? false : true;
+        this.animator.SetBool("isGround", isGround);
+
+        // ジャンプ状態では足音を消す
+        GetComponent<AudioSource>().volume = (isGround) ? 1 : 0;
+
+        // 着地状態でクリック
+        if(Input.GetMouseButtonDown(0) && isGround)
+        {
+            this.rigid2D.velocity = new Vector2(0, this.jumpVelocity);
+        }
+        // クリックをやめたら落下
+        if(Input.GetMouseButton(0) == false)
+        {
+            if(this.rigid2D.velocity.y > 0)
+            {
+                this.rigid2D.velocity *= this.dump;
+            }
+        }
+
+        // ゲームオーバー位置
+        if(transform.position.x < this.deadLine)
+        {
+            GameObject.Find("Canvas").GetComponent<UIController>().GameOver();
+            Destroy(gameObject);
+        }
+
+	}
+}
